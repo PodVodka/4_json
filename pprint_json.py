9f5925fecb3ddffabd06382b19e216ab84fef267
PodVodka <40150015+PodVodka@users.noreply.github.com>
2018-08-08 00:41:58 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -10,18 +10,19 @@ def load_data(filepath):
             return None
 
 
-def pretty_print_json(info_pprint):
-    pretty_json = json.dumps(info_pprint, indent=4, sort_keys=True, ensure_ascii=False)
-    return pretty_json
+def pretty_print_json(python_object):
+    pretty_json = json.dumps(python_object, indent=4, sort_keys=True, ensure_ascii=False)
+    print(pretty_json)
 
 
 if __name__ == '__main__':
     try:
         filepath = sys.argv[1]
-        print(pretty_print_json(load_data(filepath)))
+        python_object = load_data(filepath)
+        pretty_print_json(python_object)
     except IndexError:
         sys.exit('Отсутствуют параметры')
     except FileNotFoundError:
         sys.exit('Файл не найден')
     except ValueError:
-        sys.exit('В файле нет данных JSON')
+        sys.exit('в файле нет данных JSON')
