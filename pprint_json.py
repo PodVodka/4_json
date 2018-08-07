7fb3d6b0848d3d06f671fb20af003f1e7f5115a1
PodVodka <40150015+PodVodka@users.noreply.github.com>
2018-08-07 21:50:32 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,28 @@
 import json
+import sys
+#python pprint_json.py alko.json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_object:
+        try:
+            return json.load(file_object)
+        except json.decoder.JSONDecodeError:
+            return None
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(info_pprint):
+    pretty_json = json.dumps(info_pprint, indent=4, sort_keys=True, ensure_ascii=False)
+    return(pretty_json)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = sys.argv[1]
+        print(pretty_print_json(load_data(filepath)))
+    except IndexError:
+        sys.exit('Отсутствуют параметры')
+    except FileNotFoundError:
+        sys.exit('Файл не найден')
+    except ValueError:
+        sys.exit('в файле нет данных JSON')
